ca798b2d387fb2ba99c4a9597bf65361fd76df92
abap34 <abap0002@gmail.com>
2024-12-22 16:47:02 +0900

    feat: add printing of ast

--- src/parse/ast.jl
+++ src/parse/ast.jl
@@ -1,3 +1,5 @@
+import Base
+
 module MuAST
 
 abstract type AbstractSyntaxNode end
@@ -6,10 +8,20 @@ struct Ident <: AbstractSyntaxNode
     name::String
 end
 
+Base.show(io::IO, ident::Ident) = print(io, "`", ident.name, "`")
+
 
 struct Expr <: AbstractSyntaxNode
     head::Symbol
     args::Vector{Any} # It is not avoidable to use Any here
+end
+
+Base.show(io::IO, expr::Expr) = begin
+    print(io, "(", expr.head)
+    for arg in expr.args
+        print(io, " ", arg)
+    end
+    print(io, ")")
 end
 
 end # module MuAST
