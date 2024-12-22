e5472e3b01d2d68f4340eb071fe8fa3ef33d3420
abap34 <abap0002@gmail.com>
2024-12-23 01:49:59 +0900

    fix: fix variable name conflict

--- src/lowering/ir.jl
+++ src/lowering/ir.jl
@@ -27,13 +27,13 @@ const IR = Vector{Instr}
 
 
 
-function Base.show(io::IO, IR::IR)
+function Base.show(io::IO, ir::IR)
     println(io, "Untyped IR:")
-    idx_width = max(length(string(length(IR))), 6)
-    type_width = maximum(x -> length(string(x)), instr.irtype for instr in IR)
+    idx_width = max(length(string(length(ir))), 6)
+    type_width = maximum(x -> length(string(x)), instr.irtype for instr in ir)
     println(io, @sprintf("| %*s | %-*s | %s", idx_width, "idx", type_width, "type", "instr"))
     println(io, "| ", "-"^idx_width, " | ", "-"^type_width, " | ", "-"^20)
-    for (idx, instr) in enumerate(IR)
+    for (idx, instr) in enumerate(ir)
         println(io, @sprintf("| %*d | %-*s | %s", idx_width, idx, type_width, instr.irtype, instr.expr))
     end
 end
