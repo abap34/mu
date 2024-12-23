ee5af2fbd1b6e5473b2a6f04bed086676b70fa35
abap34 <abap0002@gmail.com>
2024-12-23 16:23:20 +0900

    refactor: abstract interpreter and implement default execution with `NaiveInterpreter`

--- src/interpreter/interpreter.jl
+++ src/interpreter/interpreter.jl
@@ -4,83 +4,12 @@ import ..MuBuiltins
 import ..MuAST
 import ..MuIR
 
+abstract type AbstractInterpreter end
 
-function run_expr(expr::MuAST.Expr, env::Dict{String, Any})
-    if expr.head == MuAST.CALL
-        f, args... = expr.args
-
-        builtin_args = run_expr.(args, Ref(env))
-
-        return MuBuiltins.run_builtin!(f.name, builtin_args, env)
-    else
-        throw("Unkonwn expr type: $expr")
-    end
-end
-
-function run_expr(expr::MuAST.Literal, env::Dict{String, Any})
-    return expr
-end
-
-function run_expr(ident::MuAST.Ident, env::Dict{String, Any})
-    return env[ident.name]
-end
-
-function interpret!(instr::MuIR.Instr, env::Dict{String, Any}, pc::Int, label_to_pc::Dict{Int, Int})
-    if instr.irtype == MuIR.CALL
-        f, args... = instr.expr.args
-
-        run_builtin!(f, args, env)
-
-        return pc + 1
-
-    elseif instr.irtype == MuIR.ASSIGN
-        ident, rhs = instr.expr.args
-
-        env[ident.name] = run_expr(rhs, env)
-
-        return pc + 1
-
-
-    elseif instr.irtype == MuIR.GOTO
-        return label_to_pc[instr.expr.args[1]]
-    
-    elseif instr.irtype == MuIR.GOTOIFNOT
-        label, cond = instr.expr.args
-
-        if !run_expr(cond, env)
-            return label_to_pc[label]
-        else
-            return pc + 1
-        end
-    elseif instr.irtype == MuIR.LABEL
-        return pc + 1
-    end
-end
-
+include("naiveinterpreter.jl")
 
 function interpret(ir::MuIR.IR)
-    label_to_pc = Dict{Int, Int}()
-    for (pc, instr) in enumerate(ir)
-        if instr.irtype == MuIR.LABEL
-            label_to_pc[instr.expr.args[1]] = pc
-        end
-    end
-
-    env = Dict{String, Any}()
-    
-    pc = 1
-    
-    while pc <= length(ir)
-        instr = ir[pc]
-        try 
-            pc = interpret!(instr, env, pc, label_to_pc)
-        catch e
-            println("Failed to interpret: $instr")
-            throw(e)
-        end
-    end
-    
-    return env
+    interpret(ir, NaiveInterpreter())
 end
 
 
---
+++ src/interpreter/naiveinterpreter.jl
@@ -0,0 +1,77 @@
+mutable struct NaiveInterpreter 
+    env::Dict{String, Any}
+    label_to_pc::Dict{Int, Int}
+    function NaiveInterpreter()
+        new(Dict{String, Any}(), Dict{Int, Int}())
+    end
+end
+
+function run_builtin!(f::String, args::AbstractArray, interp::NaiveInterpreter)
+    return MuBuiltins.get_builtin(f)(args, interp.env)
+end
+
+function run_expr(expr::MuAST.Expr, interp::NaiveInterpreter)
+    if expr.head == MuAST.CALL
+        f, args... = expr.args
+        builtin_args = run_expr.(args, Ref(interp))
+        return run_builtin!(f.name, builtin_args, interp)
+    else
+        throw("Unkonwn expr type: $expr")
+    end
+end
+
+function run_expr(expr::MuAST.Literal, interp::NaiveInterpreter)
+    return expr
+end
+
+function run_expr(ident::MuAST.Ident, interp::NaiveInterpreter)
+    return interp.env[ident.name]
+end
+
+function execute!(instr::MuIR.Instr, interp::NaiveInterpreter, pc::Int)
+    if instr.irtype == MuIR.ASSIGN
+        ident, rhs = instr.expr.args
+
+        interp.env[ident.name] = run_expr(rhs, interp)
+
+        return pc + 1
+
+    elseif instr.irtype == MuIR.GOTO
+        return interp.label_to_pc[instr.expr.args[1]]
+    
+    elseif instr.irtype == MuIR.GOTOIFNOT
+        label, cond = instr.expr.args
+
+        if !run_expr(cond, interp)
+            return interp.label_to_pc[label]
+        else
+            return pc + 1
+        end
+    elseif instr.irtype == MuIR.LABEL
+        return pc + 1
+    end
+end
+
+
+function interpret(ir::MuIR.IR, interp::NaiveInterpreter)
+    for (pc, instr) in enumerate(ir)
+        if instr.irtype == MuIR.LABEL
+            interp.label_to_pc[instr.expr.args[1]] = pc
+        end
+    end
+
+    pc = 1
+    
+    while pc <= length(ir)
+        instr = ir[pc]
+        try 
+            pc = execute!(instr, interp, pc)
+        catch e
+            println("Failed to interpret: $instr")
+            throw(e)
+        end
+    end
+    
+    return nothing
+end
+
