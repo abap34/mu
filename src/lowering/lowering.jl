22a3c91668ce91b74f5560bbd0fdb8a638d8f7c1
abap34 <abap0002@gmail.com>
2024-12-26 17:10:37 +0900

    feat: support lowering whole of programs and function

--- src/lowering/lowering.jl
+++ src/lowering/lowering.jl
@@ -19,78 +19,81 @@ var_gen(arg::MuAST.Ident) = arg
 var_gen(arg::MuAST.Literal) = arg
 var_gen(arg) = MuAST.Ident("%$(_varname_gen())")
 
-function pushlabel!(instrs::MuIR.IR, label::Int)
-    push!(instrs, Instr(MuIR.LABEL, MuAST.Expr(MuAST.LABEL, [label])))
+function pushlabel!(ir::MuIR.IR, label::Int)
+    push!(ir, Instr(MuIR.LABEL, MuAST.Expr(MuAST.LABEL, [label])))
 end
 
-function pushgoto!(instrs::MuIR.IR, label::Int)
-    push!(instrs, Instr(MuIR.GOTO, MuAST.Expr(MuAST.GOTO, [label])))
+function pushgoto!(ir::MuIR.IR, label::Int)
+    push!(ir, Instr(MuIR.GOTO, MuAST.Expr(MuAST.GOTO, [label])))
 end
 
-function pushgotoifnot!(instrs::MuIR.IR, label::Int, cond::MuAST.AbstractSyntaxNode)
-    push!(instrs, Instr(MuIR.GOTOIFNOT, MuAST.Expr(MuAST.GOTOIFNOT, [label, cond])))
+function pushgotoifnot!(ir::MuIR.IR, label::Int, cond::MuAST.AbstractSyntaxNode)
+    push!(ir, Instr(MuIR.GOTOIFNOT, MuAST.Expr(MuAST.GOTOIFNOT, [label, cond])))
 end
 
 # Direct embedding in arguments
-function add_subexpr!(expr::MuAST.Literal, myname::MuAST.Literal, instrs::MuIR.IR) end
-function add_subexpr!(expr::MuAST.Ident, myname::MuAST.Ident, instrs::MuIR.IR) end
+function add_subexpr!(expr::MuAST.Literal, myname::MuAST.Literal, ir::MuIR.IR) end
+function add_subexpr!(expr::MuAST.Ident, myname::MuAST.Ident, ir::MuIR.IR) end
 
-function add_subexpr!(expr::MuAST.Literal, myname::MuAST.Ident, instrs::MuIR.IR) 
+function add_subexpr!(expr::MuAST.Literal, myname::MuAST.Ident, ir::MuIR.IR)
     push!(
-        instrs,
+        ir,
         Instr(ASSIGN, MuAST.Expr(MuAST.ASSIGN, [myname, expr]))
     )
 end
 
 
-function add_subexpr!(expr::MuAST.Expr, myname::MuAST.Ident, instrs::MuIR.IR)
+function add_subexpr!(expr::MuAST.Expr, myname::MuAST.Ident, ir::MuIR.IR)
     f, args... = expr.args
+
 
     tmpnames = [var_gen(arg) for arg in args]
 
     for (tmpname, subexpr) in zip(tmpnames, args)
-        add_subexpr!(subexpr, tmpname, instrs)
+        add_subexpr!(subexpr, tmpname, ir)
     end
 
-    push!(instrs,
-        Instr(ASSIGN,
-            MuAST.Expr(
-                MuAST.ASSIGN,
-                [myname, MuAST.Expr(MuAST.CALL, [f, tmpnames...])]
-            )
-        )
+    assign_ast = MuAST.Expr(MuAST.ASSIGN, [myname, MuAST.Expr(MuAST.CALL, [f, tmpnames...])])
+
+    assign_instr = Instr(
+        MuIR.ASSIGN,
+        assign_ast
+    )
+
+    push!(ir,
+        assign_instr
     )
 end
 
-lowering(x::MuAST.Literal) = x
+_lowering(x::MuAST.Literal) = x
 
-function lowering(expr::MuAST.Expr)
-    instrs = MuIR.IR()
+function _lowering(expr::MuAST.Expr)
+    ir = MuIR.IR()
 
     if expr.head == MuAST.CALL
-        call_instrs = MuIR.IR()
+        call_ir = MuIR.IR()
 
-        add_subexpr!(expr, MuAST.UNUSED_IDENT, call_instrs)
+        add_subexpr!(expr, MuAST.UNUSED_IDENT, call_ir)
 
-        append!(instrs, call_instrs)
+        append!(ir, call_ir)
 
     elseif expr.head == MuAST.ASSIGN
         ident, rhs = expr.args
 
-        rhs_instrs = MuIR.IR()
+        rhs_ir = MuIR.IR()
 
-        add_subexpr!(rhs, ident, rhs_instrs)
+        add_subexpr!(rhs, ident, rhs_ir)
 
-        append!(instrs, rhs_instrs)
+        append!(ir, rhs_ir)
 
     elseif expr.head == MuAST.BLOCK
         for arg in expr.args
-            append!(instrs, lowering(arg))
+            append!(ir, _lowering(arg))
         end
 
     elseif expr.head == MuAST.PROGRAM
         for arg in expr.args
-            append!(instrs, lowering(arg))
+            append!(ir, _lowering(arg))
         end
 
     elseif expr.head == MuAST.IFELSE
@@ -113,12 +116,12 @@ function lowering(expr::MuAST.Expr)
         cond_label_id = label_gen()
         end_label_id = label_gen()
 
-        pushgotoifnot!(instrs, cond_label_id, cond)
-        append!(instrs, lowering(body))
-        pushgoto!(instrs, end_label_id)
-        pushlabel!(instrs, cond_label_id)
-        append!(instrs, lowering(elsebody))
-        pushlabel!(instrs, end_label_id)
+        pushgotoifnot!(ir, cond_label_id, cond)
+        append!(ir, _lowering(body))
+        pushgoto!(ir, end_label_id)
+        pushlabel!(ir, cond_label_id)
+        append!(ir, _lowering(elsebody))
+        pushlabel!(ir, end_label_id)
 
     elseif expr.head == MuAST.IF
         # if cond 
@@ -134,9 +137,9 @@ function lowering(expr::MuAST.Expr)
 
         cond_label_id = label_gen()
 
-        pushgotoifnot!(instrs, cond_label_id, cond)
-        append!(instrs, lowering(body))
-        pushlabel!(instrs, cond_label_id)
+        pushgotoifnot!(ir, cond_label_id, cond)
+        append!(ir, _lowering(body))
+        pushlabel!(ir, cond_label_id)
 
     elseif expr.head == MuAST.WHILE
         # while (cond) 
@@ -154,20 +157,50 @@ function lowering(expr::MuAST.Expr)
         cond_label_id = label_gen()
         end_label_id = label_gen()
 
-        pushlabel!(instrs, cond_label_id)
-        pushgotoifnot!(instrs, end_label_id, cond)
-        append!(instrs, lowering(body))
-        pushgoto!(instrs, cond_label_id)
-        pushlabel!(instrs, end_label_id)
+        pushlabel!(ir, cond_label_id)
+        pushgotoifnot!(ir, end_label_id, cond)
+        append!(ir, _lowering(body))
+        pushgoto!(ir, cond_label_id)
+        pushlabel!(ir, end_label_id)
+
+    elseif expr.head == MuAST.RETURN
+        push!(ir, Instr(MuIR.RETURN, expr)) 
 
     else
         throw(ArgumentError("Unsupported expression: $expr"))
     end
 
-    return instrs
+    return ir
 end
 
-export lowering
+# Interface of lowering
 
+function lowering(expr::MuAST.Expr)
+    if expr.head == MuAST.PROGRAM
+        lowerd = MuIR.CodeInfo[]
 
+        for function_def in expr.args
+            if function_def.head != MuAST.FUNCTION
+                throw(ArgumentError("Top-level expression must be a function definition. Got $function_def"))
+            end
 
+            name, typed_args, body = function_def.args
+
+            ir = _lowering(body)
+
+            push!(lowerd, MuIR.CodeInfo(name, typed_args, ir))
+        end
+
+        return lowerd
+
+    elseif expr.head == MuAST.FUNCTION
+        name, args, body = expr.args
+
+        ir = _lowering(body)
+
+        return MuIR.CodeInfo(name, args, ir)
+    else
+        throw(ArgumentError("Unsupported expression head. It must be either PROGRAM or FUNCTION. Got $expr"))
+    end
+
+end
