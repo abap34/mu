cb79b89a904b36dc5eccd3ba271b66d2838cb180
abap34 <abap0002@gmail.com>
2024-12-26 17:09:44 +0900

    change: rethink construction of IR
    - `IR` contain `instrs` and some information about typing.
    - `CodeInfo` contain ir and information for functions

--- src/lowering/ir.jl
+++ src/lowering/ir.jl
@@ -52,8 +52,36 @@ end
 
 
 const IR = Vector{Instr}
+# Representation of program after lowering.
+struct IR
+    instrs::Vector{Instr}
+    typed::Bool
+    function IR()
+        return new(Instr[], false)
+    end
+end
+
+struct CodeInfo
+    name::MuAST.Ident
+    args::MuAST.Expr
+    ir::IR
+    function CodeInfo(name::MuAST.Ident, args::MuAST.Expr, ir::IR)
+        @assert args.head == MuAST.FORMALARGS "args must be FORMALARGS. Got $(args)"
+        new(name, args, ir)
+    end
+end
 
 
+Base.length(ir::IR) = length(ir.instrs)
+Base.push!(ir::IR, instr::Instr) = push!(ir.instrs, instr)
+Base.pushfirst!(ir::IR, instr::Instr) = pushfirst!(ir.instrs, instr)
+function Base.append!(ir::IR, ir2::IR)
+    if ir.typed != ir2.typed
+        throw(ArgumentError("Cannot append IR with different typing status"))
+    end
+
+    append!(ir.instrs, ir2.instrs)
+end
 
 function Base.show(io::IO, ir::IR)
     println(io, "Untyped IR:")
