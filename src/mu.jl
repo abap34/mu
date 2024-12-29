3d25a0c1813177be48b23b4519df45051f3ac8fb
abap34 <abap0002@gmail.com>
2024-12-29 19:27:39 +0900

    refactor: fix import order

--- src/mu.jl
+++ src/mu.jl
@@ -3,8 +3,9 @@ module mu
 module MuCore
 
 include("parse/parse.jl")
-include("lowering/lowering.jl")
 include("builtin/builtin.jl")
+
+include("lowering/lowering.jl")
 include("interpreter/interpreter.jl")
 
 end # module MuCore
