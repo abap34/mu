a4147172b02eb895d4a08e9c49fe770f6884e857
abap34 <abap0002@gmail.com>
2024-12-31 02:10:44 +0900

    change: don't implement `injection!` for all `AbstractInterpreter`

--- src/interpreter/interpreter.jl
+++ src/interpreter/interpreter.jl
@@ -8,10 +8,6 @@ abstract type AbstractInterpreter end
 
 include("concreate_interpreter.jl")
 
-function injection!(interp::AbstractInterpreter, codeinfo::MuIR.CodeInfo)
-    add_method!(interp.methodtable, codeinfo)
-end
-
 function interpret(ir::MuIR.IR)
     interpret(ir, ConcreateInterpreter())
 end
