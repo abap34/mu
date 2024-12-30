070d68b6687dc51404ae6e93429b6ac2c64d8692
abap34 <abap0002@gmail.com>
2024-12-31 01:05:11 +0900

    feat: add interface to load function for interpreter

--- src/interpreter/interpreter.jl
+++ src/interpreter/interpreter.jl
@@ -6,11 +6,15 @@ import ..MuIR
 
 abstract type AbstractInterpreter end
 
+
+function injection!(interp::ConcreateInterpreter, codeinfo::MuIR.CodeInfo)
+    add_method!(interp.methodtable, codeinfo)
+ end
+
 include("concreate_interpreter.jl")
 
 function interpret(ir::MuIR.IR)
     interpret(ir, ConcreateInterpreter())
 end
-
 
 end # module MuInterpreter
