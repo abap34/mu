c933a56c702e8e2ec8e3ab5d95aaf455e27c205d
abap34 <abap0002@gmail.com>
2025-01-11 20:59:28 +0900

    refactor: add error message

--- src/builtin/builtin.jl
+++ src/builtin/builtin.jl
@@ -29,8 +29,8 @@ end
 function set_constant!(name::String, input::AbstractArray, output::DataType)
     @assert all(x -> x isa DataType, input)
     @assert output <: MuTypes.MuType
-    @assert !haskey(TFUNCS, name)
-    @assert haskey(BUILTINS, name)
+    @assert !haskey(TFUNCS, name) "TFunc $name already exists"
+    @assert haskey(BUILTINS, name) "Builtin $name does not exist"
     TFUNCS[name] = function (argtypes::AbstractArray)
         for (i, (argtype, expected)) in enumerate(zip(argtypes, input))
             if !(MuTypes.issubtype(argtype, expected))
