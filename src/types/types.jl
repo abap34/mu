752299ccac9bb0e41dea132f11827b7f09341b60
abap34 <abap0002@gmail.com>
2025-01-18 21:59:25 +0900

    feat: add `Tuple`

--- src/types/types.jl
+++ src/types/types.jl
@@ -30,8 +30,9 @@ struct Bool <: MuType end
 struct AbstractString <: MuType end
 struct String <: MuType end
 struct AbstractArray <: MuType end
-struct Array{T<:MuType,N} <: MuType end
-struct Union{S<:MuType,T<:MuType} <: MuType end
+struct Array{T <: MuType, N} <: MuType end
+struct Union{S <: MuType, T <: MuType} <: MuType end
+struct Tuple{S <: MuType, T <: MuType} <: MuType end
 struct Bottom <: MuType end
 
 
@@ -60,49 +61,143 @@ supertype(::Type{Bool}) = Number
 supertype(::Type{AbstractString}) = Any
 supertype(::Type{String}) = AbstractString
 supertype(::Type{AbstractArray}) = Any
-supertype(::Type{Array{T,N}}) where {T,N} = AbstractArray
-supertype(::Type{Union{S,T}}) where {S,T} = Any
+supertype(::Type{Array{T, N}}) where {T, N} = AbstractArray
+supertype(::Type{Union{S, T}}) where {S, T} = Any
+supertype(::Type{Tuple{S, T}}) where {S, T} = Any
 
 # Supertype of Bottom is all of concrete types, makes implementation complex. For now, throw an error.
 supertype(::Type{Bottom}) = throw(DomainError("Cannot get supertype of `Bottom`` because of infinite types are there."))
 
 
 
-isunion(::Type{Union{S,T}}) where {S,T} = true
-isunion(::Type{T}) where {T<:MuType} = false
+isunion(::Type{Union{S, T}}) where {S, T} = true
+isunion(::Type{T}) where {T <: MuType} = false
+
+function uniontype(types::Base.AbstractArray)
+    if isempty(types)
+        return Union
+    end
+
+    if length(types) == 1
+        return Union{types[1]}
+    end
+
+    t = Union{types[1], types[2]}
+
+    for i in eachindex(types)[3:end]
+        t = Union{t, types[i]}
+    end
+
+    return t
+end
+
+uniontype(t::Base.Union) = Union{_type_to_mutype(t.a), uniontype(t.b)}
+uniontype(t::DataType) = _type_to_mutype(t)
+
+function tupletype(types::Base.AbstractArray, T)
+    if isempty(types)
+        return Tuple
+    end
+
+    if length(types) == 1
+        return Tuple{types[1]}
+    end
+
+    t = Tuple{types[1], types[2]}
+
+    for i in eachindex(types)[3:end]
+        t = Tuple{t, types[i]}
+    end
+
+    return t
+end
+
+tupletype(t::Base.Tuple) = Tuple{_type_to_mutype(t.a), tupletype(t.b)}
+tupletype(t::DataType) = _type_to_mutype(t)
 
 
 # Expand Union type.
 # e.g. expand_types(Union{Int, Union{Float, Bool}}) => [Int, Float, Bool]
-expand_types(::Type{Union{S,T}}) where {S,T} = [expand_types(S); expand_types(T)]
-expand_types(::Type{T}) where {T<:MuType} = [T]
+expand_types(::Type{Union{S, T}}) where {S, T} = [expand_types(S); expand_types(T)]
+expand_types(::Type{Union{S}}) where {S} = [expand_types(S)]
+expand_types(::Type{Union}) = []
 
+expand_types(::Type{T}) where {T <: MuType} = [T]
+
+# Get component types of Tuple type.
+# e.g. component_types(Tuple{Int, Float}) => [Int, Float]
+#
+# Note: separate function from `expand_types` because of avoid this behavior: 
+# expand_types(Union{Tuple{Int, Float}, Tuple{Bool, String}}) => [Int, Float, Bool, String]
+component_types(::Type{Tuple{S, T}}) where {S, T} = [S, T]
+component_types(::Type{Tuple{S}}) where {S} = [S]
+component_types(::Type{Tuple}) = []
+component_types(::Type{T}) where {T <: MuType} = [T]
+
+# Normalize Union type.
+# In normalized form, `Union` is only appear at the top level.
+_normalize(::Type{T}) where {T <: MuType} = T
+
+
+function _normalize(::Type{Tuple{Union{S1, S2}, Union{T1, T2}}}) where {S1, S2, T1, T2} 
+    return Union{_normalize(Tuple{S1, T1}), _normalize(Tuple{S1, T2}), _normalize(Tuple{S2, T1}), _normalize(Tuple{S2, T2})}
+end
+
+
+_normalize(::Type{Tuple{Union{S, T}, U}}) where {S, T, U} = Union{_normalize(Tuple{S, U}), _normalize(Tuple{T, U})}
+_normalize(::Type{Tuple{S, Union{T, U}}}) where {S, T, U} = Union{_normalize(Tuple{S, T}), _normalize(Tuple{S, U})}
+                                                                                                                                                                                                                                                                                                                                                                              
+_normalize(::Type{Union{S, T}}) where {S <: MuType, T <: MuType} = Union{_normalize(S), _normalize(T)}
+
+normalize(::Type{T}) where {T <: MuType} = _normalize(T)
 
 
 # t1 <: t2
+
+# Bottom <: t for all t
 issubtype(::Type{Bottom}, ::Type{Bottom}) = true
 issubtype(::Type{Bottom}, ::Type{<:MuType}) = true
 issubtype(::Type{Bottom}, ::Type{<:Union}) = true
 
+# t <: Any for all t
 issubtype(::Type{Any}, ::Type{Any}) = true
 issubtype(::Type{Any}, ::Type{<:MuType}) = false
 issubtype(::Type{Any}, ::Type{<:Union}) = false
 issubtype(::Type{<:Union}, ::Type{Any}) = true
 
-issubtype(::Type{U1}, ::Type{U2}) where {U1<:Union,U2<:Union} = all(issubtype(u, U2) for u in expand_types(U1))
-issubtype(::Type{U}, ::Type{T}) where {T<:MuType,U<:Union} = all(issubtype(u, T) for u in expand_types(U))
-issubtype(::Type{T}, ::Type{U}) where {T<:MuType,U<:Union} = any(issubtype(T, u) for u in expand_types(U))
+# U1 <: U2 ⇔ ∀u ∈ U1, u <: U2
+issubtype(::Type{U1}, ::Type{U2}) where {U1 <: Union, U2 <: Union} = all(issubtype(u, U2) for u in expand_types(U1))
+# U <: T ⇔ ∀u ∈ U, u <: T
+issubtype(::Type{U}, ::Type{T}) where {T <: MuType, U <: Union} = all(issubtype(u, T) for u in expand_types(U))
+# T <: U ⇔ ∃u ∈ U, T <: u
+issubtype(::Type{T}, ::Type{U}) where {T <: MuType, U <: Union} = any(issubtype(T, u) for u in expand_types(U))
+# S <: T, if there is a chain of subtypes from S to T.
+issubtype(::Type{S}, ::Type{T}) where {S <: MuType, T <: MuType} = issubtype(supertype(S), T) || S == T
 
-issubtype(::Type{S}, ::Type{T}) where {S<:MuType,T<:MuType} = issubtype(supertype(S), T) || S == T
 
-jointype(::Type{S}, ::Type{T}) where {S<:MuType,T<:MuType} = issubtype(S, T) ? T : issubtype(T, S) ? S : Union{S,T}
+# Tuple{S} <: Tuple{T} ⇔ 
+#  1. #S = #T
+#  2. ∀i, Sᵢ <: Tᵢ
+function issubtype(S::Type{Tuple{S1, T1}}, T::Type{Tuple{S2, T2}}) where {S1 <: MuType, T1 <: MuType, S2 <: MuType, T2 <: MuType}
+    (length(component_types(S)) != length(component_types(T))) && return false
 
-function meettype(::Type{T}, ::Type{U}) where {T<:MuType,U<:MuType}
+    return all(issubtype(s, t) for (s, t) in zip(component_types(S), component_types(T)))
+end
+
+issubtype(::Type{Tuple{S}}, ::Type{T}) where {S <: MuType, T <: MuType} = false
+issubtype(::Type{S}, ::Type{Tuple{T}}) where {S <: MuType, T <: MuType} = false
+
+
+
+
+jointype(::Type{S}, ::Type{T}) where {S <: MuType, T <: MuType} = issubtype(S, T) ? T : issubtype(T, S) ? S : Union{S, T}
+
+function meettype(::Type{T}, ::Type{U}) where {T <: MuType, U <: MuType}
     if issubtype(T, U)
         return T
     elseif issubtype(U, T)
         return U
-    elseif isunion(T) 
+    elseif isunion(T)
         return reduce(jointype, (meettype(t, U) for t in expand_types(T)))
     elseif isunion(U)
         return meettype(U, T)
@@ -111,151 +206,9 @@ function meettype(::Type{T}, ::Type{U}) where {T<:MuType,U<:MuType}
     end
 end
 
+Base.:(==)(::Type{U1}, ::Type{U2}) where {U1 <: Union, U2 <: Union} = Set(expand_types(U1)) == Set(expand_types(U2))
 
-
-Base.:(==)(::Type{U1}, ::Type{U2}) where {U1<:Union,U2<:Union} = Set(expand_types(U1)) == Set(expand_types(U2))
-
-const NON_PARMETRIC_TYPES_STR = ["Any", "Number", "Real", "Int", "Float", "Bool", "AbstractString", "String", "AbstractArray", "Bottom"]
-
-
-function _str_to_type(name::Base.String)
-    @assert name in NON_PARMETRIC_TYPES_STR "Type name must be one of $(NON_PARMETRIC_TYPES_STR). Got $name"
-    if name == "Any"
-        return Any
-    elseif name == "Number"
-        return Number
-    elseif name == "Real"
-        return Real
-    elseif name == "Int"
-        return Int
-    elseif name == "Float"
-        return Float
-    elseif name == "Bool"
-        return Bool
-    elseif name == "AbstractString"
-        return AbstractString
-    elseif name == "String"
-        return String
-    elseif name == "AbstractArray"
-        return AbstractArray
-    elseif name == "Bottom"
-        return Bottom
-    else
-        throw(ArgumentError("Unknown type: $name"))
-    end
-end
-
-function shorten_str(t::DataType)
-    if t <: Array
-        return "Array{" * shorten_str(t.parameters[1]) * ", " * string(t.parameters[2]) * "}"
-    elseif t <: Union
-        return "Union{" * join([shorten_str(x) for x in expand_types(t)], ", ") * "}"
-    elseif t == Any
-        return "Any"
-    elseif t == Number
-        return "Number"
-    elseif t == Real
-        return "Real"
-    elseif t == Int
-        return "Int"
-    elseif t == Float
-        return "Float"
-    elseif t == Bool
-        return "Bool"
-    elseif t == AbstractString
-        return "AbstractString"
-    elseif t == String
-        return "String"
-    elseif t == AbstractArray
-        return "AbstractArray"
-    elseif t == Bottom
-        return "Bottom"
-    
-    else
-        return string(t)
-    end
-end
-
-function _type_to_mutype(t::DataType)
-    if t == Base.Any
-        return Any
-    elseif t == Base.Number
-        return Number
-    elseif t == Base.Real
-        return Real
-    elseif t == Base.Int
-        return Int
-    elseif t == Base.Float64
-        return Float
-    elseif t == Base.Bool
-        return Bool
-    elseif t == Base.String
-        return String
-    elseif t == Base.AbstractString
-        return AbstractString
-    elseif t == Base.AbstractArray
-        return AbstractArray
-    elseif t == Base.Bottom
-        return Bottom
-    elseif t isa Array
-        return Array{_type_to_mutype(eltype(t)), ndims(t)}
-    else
-        throw(ArgumentError("Unknown type: $t"))
-    end
-end
-
-function astype(type::MuAST.Ident)
-    @assert type.name in NON_PARMETRIC_TYPES_STR "Type name must be one of $(NON_PARMETRIC_TYPES_STR). Got $(type.name)"
-    return _str_to_type(type.name)
-end
-
-
-function astype(type::MuAST.Expr)
-    @assert type.head == MuAST.TYPE "`astype` can only be applied to `TYPE` expression. Got $(type.head)"
-
-    name = type.args[1].name
-    params = type.args[2:end]
-
-    if name in NON_PARMETRIC_TYPES_STR
-        @assert isempty(params) "Type $name does not take any parameters. Got $(length(params))"
-        return _str_to_type(name)
-
-    elseif name == "Array"
-        elemtype = astype(params[1])
-        dim = type.args[3]
-
-        return Array{elemtype, dim}
-    elseif name == "Union"
-        @assert length(params) >= 2 "Union type must have at least 2 types. Got $(length(type.args))"
-
-        t = Union{astype(pop!(params)), astype(pop!(params))}
-
-        
-        while !isempty(params)
-            t = Union{astype(pop!(params)), t}
-        end
-
-        return t
-    else
-        throw(ArgumentError("Unknown type: $name"))
-    end
-end
-
-function typeof(v)
-    if isa(v, Base.Bool)
-        return Bool
-    elseif isa(v, Base.Int)
-        return Int
-    elseif isa(v, Base.Float64)
-        return Float
-    elseif isa(v, Base.String)
-        return String
-    elseif isa(v, Base.Array)
-        return Array{_type_to_mutype(eltype(v)), ndims(v)}
-    else
-        throw(ArgumentError("Unknown Value!: $v $(Base.typeof(v)). Only Int, Float64, String, Bool, Array are supported."))
-    end
-end
+include("conversion.jl")
 
 
 export MuType
